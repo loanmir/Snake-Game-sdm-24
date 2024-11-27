24e4f3d8bdb7bf715fa5b07593007fb4c2a40681
francescacraievich <franci.craievich2000@gmail.com>
2024-11-27 13:30:53 +0100

    test body collision + currentDirection, ignore movement of the head turning back on the same position

--- src/main/java/SnakeMovement.java
+++ src/main/java/SnakeMovement.java
@@ -1,6 +1,7 @@
 public class SnakeMovement {
 
     private final Board board;
+    private Direction currentDirection = Direction.NULL;
 
     public SnakeMovement(Board board) {
         this.board = board;
@@ -26,33 +27,57 @@ public class SnakeMovement {
 
     // move the head up one and put an S where the H was
     public void moveHeadUp() {
+        if (currentDirection == Direction.DOWN) {
+            return; // ignore, cannot move in this direction
+        }
         int[] rowColHead = board.getRowColHead();
         board.setCell(rowColHead[0], rowColHead[1], 'S');
         rowColHead[0]++;
         board.setCell(rowColHead[0], rowColHead[1], 'H');
+        currentDirection = Direction.UP;
     }
 
     // move the head down one and put an S where the H was
     public void moveHeadDown() {
+        if (currentDirection == Direction.UP) {
+            return; //  ignore, cannot move in this direction
+        }
         int[] rowColHead = board.getRowColHead();
         board.setCell(rowColHead[0], rowColHead[1], 'S');
         rowColHead[0]--;
         board.setCell(rowColHead[0], rowColHead[1], 'H');
+        currentDirection = Direction.DOWN;
     }
 
     // move the head left one and put an S where the H was
     public void moveHeadLeft() {
+        if (currentDirection == Direction.RIGHT) {
+            return; // ignore, cannot move in this direction
+        }
         int[] rowColHead = board.getRowColHead();
         board.setCell(rowColHead[0], rowColHead[1], 'S');
         rowColHead[1]--;
         board.setCell(rowColHead[0], rowColHead[1], 'H');
+        currentDirection = Direction.LEFT;
     }
 
     // move the head right one and put an S where the H was
     public void moveHeadRight() {
+        if (currentDirection == Direction.LEFT) {
+            return; // ignore, cannot move in this direction
+        }
         int[] rowColHead = board.getRowColHead();
         board.setCell(rowColHead[0], rowColHead[1], 'S');
         rowColHead[1]++;
         board.setCell(rowColHead[0], rowColHead[1], 'H');
+        currentDirection = Direction.RIGHT;
+    }
+
+    public Direction getCurrentDirection() {
+        return currentDirection;
+    }
+    public void setCurrentDirection(Direction direction) {
+        this.currentDirection = direction;
     }
 }
+
--- src/test/java/GameOverConditions.java
+++ src/test/java/GameOverConditions.java
@@ -7,22 +7,37 @@ public class GameOverConditions {
     @Test
     @Disabled
     void deathByBorderCollision(){
-        // Posizioniamo la testa dello Snake nell'angolo in alto a sinistra
+        //head of the snake up and left
         Board board = new Board("");
-        board.setCell(0, 0, 'H'); // La testa è in alto a sinistra
+        board.setCell(0, 0, 'H');
         SnakeMovement snakeMovement = new SnakeMovement(board);
 
-        // Muoviamo la testa verso l'alto
+        // moving the head up
         snakeMovement.moveHeadUp();
 
-        // Verifichiamo se il gioco è finito
+        // verify if the game is finished
         assertTrue(snakeMovement.isGameOver());
     }
 
     @Test
     @Disabled
     void deathBySelfCollision(){
-    //devo ancora farlo :)
+                   Board board = new Board("");
+            board.setCell(4, 4, 'H');  // head
+            board.setCell(4, 3, 'S');  // body
+            board.setCell(5, 3, 'S');  // body
+            board.setCell(5, 4, 'S');  // body
 
-    }
+            SnakeMovement snakeMovement = new SnakeMovement(board);
+
+
+            snakeMovement.moveHeadLeft(); // head on (4, 3)
+            assertFalse(snakeMovement.isGameOver()); //ignore -- not valid movement
+
+            snakeMovement.moveHeadDown(); // head on (5, 4) --> body collision
+            assertTrue(snakeMovement.isGameOver());
+        }
+
+
+
 }
