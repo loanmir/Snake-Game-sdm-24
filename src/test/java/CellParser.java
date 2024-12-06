f0cb3b67682d09245d2345cf119b0a73885d1078
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-12-06 11:55:24 +0100

    Tests on getCell

---
+++ src/test/java/CellParser.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CellParser {
+    @Test
+    void cellHasBlank() {
+        Board board = new Board("");
+        assertEquals(board.getCell(1,1), Cell.BLANK);
+    }
+
+    @Test
+    void cellHasFood() {
+        Board board = new Board("");
+        assertEquals(board.getCell(5,6), Cell.FOOD);
+    }
+
+    @Test
+    void cellHasHead() {
+        Board board = new Board("");
+        assertEquals(board.getCell(5,5), Cell.HEAD);
+    }
+
+    @Test
+    void cellHasWall() {
+        Board board = new Board("");
+        assertEquals(board.getCell(0,0), Cell.WALL);
+    }
+
+    @Test
+    void cellHasBody() {
+        Board board = new Board("");
+        assertEquals(board.getCell(5,4), Cell.BODY);
+    }
+}
